1c6524670d86c1457553b2a3978a36634f0749b3
Onur Hacıosmanoğlu <onrhaci90@gmail.com>
2025-01-14 22:16:27 +0300

    - CustomerService ve DepartmentService için scoped olarak bağımlılık enjeksiyonu eklendi. - DatabaseSettings yapılandırmaları yapıldı ve appsettings.json'dan okuma işlemi düzenlendi. - AutoMapper entegrasyonu için gerekli konfigürasyonlar eklendi. - IDatabaseSettings için bağımlılık enjeksiyonu sağlandı.

--- MyMongoProjecktNight/Entities/Department.cs
+++ MyMongoProjecktNight/Entities/Department.cs
@@ -1,7 +1,12 @@
-﻿namespace MyMongoProjecktNight.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyMongoProjecktNight.Entities
 {
     public class Department
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string DepartmentId { get; set; }
         public string DepartmentName { get; set; }
     }
--- MyMongoProjecktNight/Program.cs
+++ MyMongoProjecktNight/Program.cs
@@ -1,6 +1,30 @@
+using Microsoft.Extensions.Options;
+using MyMongoProjecktNight.Services;
+using MyMongoProjecktNight.Services.DepartmentServices;
+using MyMongoProjecktNight.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// ICustomerService arayüzünü implement eden CustomerService sýnýfýný scoped olarak kaydeder.
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+// IDepartmentService arayüzünü implement eden DepartmentService sýnýfýný scoped olarak kaydeder.
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+// AutoMapper'ý uygulamaya ekler ve otomatik çalýþmasýný saðlar.
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+// DatabaseSettings ayarlarýný "DatabaseSettingsKey" baþlýðý altýndaki yapýlandýrmalardan çeker.
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettingsKey"));
+// IDatabaseSettings için baðýmlýlýk enjeksiyonu saðlar.
+// IOptions<DatabaseSettings> kullanýlarak yapýlandýrmadan çekilen ayarlarýn bir örneði alýnýr.
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    // IOptions<DatabaseSettings>'den ayarlarý alýp IDatabaseSettings olarak döner.
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
